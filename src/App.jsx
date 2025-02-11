4556ff94ce5d385e985c1da31e59ffa2e7032eae
Shapin199 <mdtanvir054321@gmail.com>
2025-02-11 19:23:29 +0600

    add tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './Style.css'
+
+function App() {
+  
+
+  return (
+    <div>
+        <h1 className="text-red-500">
+          Sign In
+        </h1>
+    </div>
+  )
+}
+
+export default App
