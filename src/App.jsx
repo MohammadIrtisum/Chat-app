78fc534623e979d3da0cef397f781addef2507bd
Shapin199 <mdtanvir054321@gmail.com>
2025-02-15 12:45:54 +0600

    form create

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,68 @@
 
 
 
+import { IconLock, IconMail } from '@tabler/icons-react'
 import { Button } from './components/Button'
+import { Text } from './components/Text'
+import { TextInput } from './components/TextInput'
 import './Style.css'
+import { PasswordInput } from './components/PasswordInput'
+import { Anchor } from './components/Anchor'
+import { Heading } from './components/Heading'
 
 function App() {
   
 
   return (
-    <div className="m-20 space-y-4">
-        {/* <h1 className="text-red-500">
+    <div className="m-20 space-y-8">
+      <div className='max-w-md space-y-4'>
+        <Text size='x1' weight='bold'>
           Sign In
-        </h1> */}
+        </Text>
 
-        {/* <Button veriant="outline" >Learn more</Button> */}
-        <Button onClick={()=>alert("hello world")}>Sing up</Button>
-        <Button >Sing in</Button>
-        <Button variant='outline' className="m-20 "  >Learn more</Button>
+        <TextInput
+        Iabel ="Email"
+        placeholder='your@email.com'
+        icon={<IconMail className='w-5 h-5 text-gray-400'/>}
+        ></TextInput>
+
+        <PasswordInput 
+        label='Password'
+        placeholder='Enter your password'
+        error='Password must be at least 8 characters'
+        icon={<IconLock className='w-5 h-5 text-gray-400'></IconLock>}
+        ></PasswordInput>
+        <Button className='w-full'>Sign In</Button>
+        <div className='text-center'>
+          <text size="sm" className='text-gray-600'>
+          Don't have an account? <Anchor></Anchor>
+          </text>
+        </div>
         
+      </div>
+
+      <div className="space-y-4">
+        <Text size="lg">Button Variants</Text>
+        <div className="space-x-4">
+          <Button>Default Button</Button>
+          <Button variant="outline">Outline Button</Button>
+          <Button variant="light">Light Button</Button>
+          <Button disabled>Disabled Button</Button>
+        </div>
+      </div>
+
+      <div className="space-y-4">
+        <Text size="lg">Heading Examples</Text>
+        <div className="space-y-2">
+          <Heading type={1}>Heading 1</Heading>
+          <Heading type={2}>Heading 2</Heading>
+          <Heading type={3}>Heading 3</Heading>
+          <Heading type={4}>Heading 4</Heading>
+          <Heading type={5}>Heading 5</Heading>
+          <Heading type={6}>Heading 6</Heading>
+        </div>
+      </div>
+       
     </div>
   )
 }
