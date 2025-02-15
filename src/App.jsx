9070a3486a0df420ddd52c985c36b0ebfa3614aa
Shapin199 <mdtanvir054321@gmail.com>
2025-02-15 10:23:15 +0600

    done Button components using clsx and tm-marge also jsdoc

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 
+
+
+import { Button } from './components/Button'
 import './Style.css'
 
 function App() {
   
 
   return (
-    <div>
-        <h1 className="text-red-500">
+    <div className="m-20 space-y-4">
+        {/* <h1 className="text-red-500">
           Sign In
-        </h1>
+        </h1> */}
+
+        {/* <Button veriant="outline" >Learn more</Button> */}
+        <Button onClick={()=>alert("hello world")}>Sing up</Button>
+        <Button >Sing in</Button>
+        <Button variant='outline' className="m-20 "  >Learn more</Button>
+        
     </div>
   )
 }
